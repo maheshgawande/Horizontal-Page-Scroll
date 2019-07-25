ec5639a00ad9df756fcbf89a775c6cd2f1bee5a4
mahesh gawande <maheshsg297@gmail.com>
2019-07-25 13:37:21 +0530

    Added functionality to arrow button that scrolls to next slide. (Edited app.js)

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
-// First commit through vs-code
+const img = document.querySelector('img');
+const outer_wrapper = document.getElementById('outer_wrapper');
+const wrapper = document.getElementById('wrapper');
+
+let scrollable = wrapper.scrollWidth - document.documentElement.scrollWidth;
+i = 1;
+
+img.addEventListener('click', () => {
+    let scrolled = document.documentElement.scrollWidth * i;
+    outer_wrapper.scrollTo(0, scrolled);
+    i++;
+});
