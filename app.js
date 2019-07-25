2d2e99ff1b35366066662438c6407f7334b1b28c
mahesh gawande <maheshsg297@gmail.com>
2019-07-25 18:56:52 +0530

    Previous functionality removed. New functionality added.

--- app.js
+++ app.js
@@ -1,12 +1,50 @@
-const img = document.querySelector('img');
-const outer_wrapper = document.getElementById('outer_wrapper');
+const img = document.querySelectorAll('img');
+const left_arrow = document.getElementById('left-arrow');
+const right_arrow = document.getElementById('right-arrow')
+const outer_wrapper = document.getElementById('outer-wrapper');
 const wrapper = document.getElementById('wrapper');
 
+const initial_doc_width = document.documentElement.scrollWidth;
 let scrollable = wrapper.scrollWidth - document.documentElement.scrollWidth;
-i = 1;
+let scrolled = 0;
 
-img.addEventListener('click', () => {
-    let scrolled = document.documentElement.scrollWidth * i;
-    outer_wrapper.scrollTo(0, scrolled);
-    i++;
+outer_wrapper.addEventListener('scroll', () => {
+    scrolled = outer_wrapper.scrollTop;
+    if (scrolled == 0) {
+        img[0].style.opacity = '.5';
+        left_arrow.style.pointerEvents = 'none';
+    } else if (scrolled > 0 && scrolled < initial_doc_width * 3) {
+        img[0].style.opacity = '1';
+        left_arrow.style.pointerEvents = 'all';
+        img[1].style.opacity = '1';
+        right_arrow.style.pointerEvents = 'all';
+    } else if (scrolled > 0 && scrolled == initial_doc_width *3 ) {
+        img[1].style.opacity = '.5';
+        right_arrow.style.pointerEvents = 'none';
+    }
+});
+
+left_arrow.addEventListener('click', () => {
+    if (scrolled > 0 && scrolled <= initial_doc_width) {
+        outer_wrapper.scrollTo(0, 0);
+    } else if (scrolled > initial_doc_width && scrolled <= initial_doc_width * 2) {
+        outer_wrapper.scrollTo(0, initial_doc_width);
+    } else if (scrolled > initial_doc_width * 2 && scrolled <= initial_doc_width * 3) {
+        outer_wrapper.scrollTo(0, initial_doc_width * 2);
+    } else if (scrolled == initial_doc_width * 3) {
+        outer_wrapper.scrollTo(0, initial_doc_width * 2);
+    } else {
+        console.log('error!');
+    }
+});
+right_arrow.addEventListener('click', () => {
+    if (scrolled >= 0 && scrolled < initial_doc_width) {
+        outer_wrapper.scrollTo(0, initial_doc_width)
+    } else if (scrolled >= initial_doc_width && scrolled < initial_doc_width * 2) {
+        outer_wrapper.scrollTo(0, initial_doc_width * 2)
+    } else if (scrolled >= initial_doc_width * 2 && scrolled < initial_doc_width * 3) {
+        outer_wrapper.scrollTo(0, initial_doc_width * 3)
+    } else {
+        console.log('error!');
+    }
 });
